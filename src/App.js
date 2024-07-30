47a91e3550ab436cd836763142cc1d54f7efbf11
zellkon <zellkon@live.com>
2024-07-30 20:11:16 +0930

    init demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import CKEditorComponent from './components/CKEditorComponent';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const { handleSubmit, control } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+
+    return (
+        <div>
+            <h1>CKEditor 5 with react-hook-form and Custom Toolbar - ZELLKON</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <Controller
+                        name="content"
+                        control={control}
+                        defaultValue=""
+                        render={({ field: { onChange, value } }) => (
+                            <CKEditorComponent value={value} onChange={onChange} />
+                        )}
+                    />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+};
 
 export default App;
